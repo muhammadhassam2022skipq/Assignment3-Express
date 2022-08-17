eb549889ac9b51185af100bc2d8581d2eb1ebbba
Muhammad Hassam <107360982+muhammadhassam2022skipq@users.noreply.github.com>
2022-08-17 16:01:43 +0500

    Add files via upload

---
+++ Angular Project/angular-assignment/src/app/accounts/signin/signin-from/signin-from.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signin-from',
+  templateUrl: './signin-from.component.html',
+  styleUrls: ['./signin-from.component.css']
+})
+
+
+export class SigninFromComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSigninSubmit() {
+
+  }
+
+}
